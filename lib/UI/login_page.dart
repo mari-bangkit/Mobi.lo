325463a29faf48ce890afb62dca271d8be0769a5
aldanrp <rallfirdan@gmail.com>
2021-12-01 09:07:31 +0700

    Login page

--- lib/UI/login_page.dart
+++ lib/UI/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:yuk_mancing/Constant/style.dart';
 
@@ -133,18 +135,151 @@ class _LoginPageState extends State<Loginpage> {
                             ),
                           ),
                           Container(
-                            height: 23,
-                            margin: const EdgeInsets.only(right: 20, top: 3),
+                            height: 40,
+                            margin: const EdgeInsets.only(right: 15, top: 3),
                             alignment: Alignment.bottomRight,
-                            child: const Text(
-                              "Forgot Password ?",
-                              style: TextStyle(
-                                  fontFamily: "Monstserrat",
-                                  fontSize: 15,
-                                  color: kLightGray,
-                                  fontWeight: FontWeight.w600),
+                            child: TextButton(
+                              onPressed: () {
+                                print("dialihkan ke forgot password");
+                              },
+                              child: const Text(
+                                "Forgot Password ?",
+                                style: TextStyle(
+                                    fontFamily: "Monstserrat",
+                                    fontSize: 15,
+                                    color: kLightGray,
+                                    fontWeight: FontWeight.w600),
+                              ),
                             ),
                           ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(
+                              right: 20,
+                            ),
+                            padding: const EdgeInsets.all(2),
+                            height: 44,
+                            width: MediaQuery.of(context).size.height,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                padding: const EdgeInsets.all(10),
+                                minimumSize: Size.fromWidth(
+                                    MediaQuery.of(context).size.width / 3),
+                                primary: kBlack,
+                                backgroundColor: kSecondary,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                              ),
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                  color: kBlack,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Don’t have acount ?",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: kLightGray,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Container(
+                                height: 40,
+                                alignment: Alignment.topCenter,
+                                child: TextButton(
+                                  onPressed: () {
+                                    print("dialihkan ke forgot password");
+                                  },
+                                  child: const Text(
+                                    "Sign up",
+                                    style: TextStyle(
+                                        fontFamily: "Monstserrat",
+                                        fontSize: 15,
+                                        color: kBlack,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width / 3,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 0.8,
+                                    color: kLightGray.withOpacity(0.5),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  right: 5,
+                                  left: 5,
+                                ),
+                                child: const Text(
+                                  "Or login with",
+                                  style: TextStyle(
+                                    fontFamily: "Monstserrat",
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15,
+                                    color: kLightGray,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width / 3,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 0.8,
+                                    color: kLightGray.withOpacity(0.5),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                color: kLightred,
+                                onPressed: () {},
+                                icon: const ImageIcon(
+                                  AssetImage(
+                                    "Assets/Icons/flat-color-icons_google.png",
+                                  ),
+                                ),
+                                iconSize: 25,
+                              ),
+                              IconButton(
+                                color: kLighBlue,
+                                onPressed: () {},
+                                icon: const ImageIcon(
+                                  AssetImage(
+                                    "Assets/Icons/ant-design_facebook-filled.png",
+                                  ),
+                                ),
+                                iconSize: 25,
+                              ),
+                            ],
+                          )
                         ],
                       ),
                     ),
