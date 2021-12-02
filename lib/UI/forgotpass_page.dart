7cec9fb6c4b1e9c72885a2d977bf0b4590992c34
aldanrp <rallfirdan@gmail.com>
2021-12-02 10:46:06 +0700

    add signup and forgot password

--- lib/UI/forgotpass_page.dart
+++ lib/UI/forgotpass_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:yuk_mancing/Constant/style.dart';
+import 'package:yuk_mancing/UI/login_page.dart';
 
 class ForgotPass extends StatelessWidget {
   const ForgotPass({Key? key}) : super(key: key);
@@ -7,7 +10,144 @@ class ForgotPass extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          margin: const EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 20,
+          ),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Loginpage(),
+                        ),
+                      );
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      size: 30,
+                      color: kBlack,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  const Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                "Email",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: kLightGray.withOpacity(0.3),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email or Phone number"),
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: kGray,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(2),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: TextButton(
+                  onPressed: () {
+                    print("data di submit");
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    minimumSize:
+                        Size.fromWidth(MediaQuery.of(context).size.width / 3),
+                    primary: kBlack,
+                    backgroundColor: kSecondary,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: kBlack,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 40,
+                margin: const EdgeInsets.only(top: 2),
+                alignment: Alignment.center,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: kWhite,
+                  ),
+                  onPressed: () {
+                    print("dialihkan ke forgot password");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Loginpage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Back to login",
+                    style: TextStyle(
+                        fontFamily: "Monstserrat",
+                        fontSize: 15,
+                        color: kLightGray,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/UI/signup_page.dart
+++ lib/UI/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yuk_mancing/Constant/style.dart';
+import 'package:yuk_mancing/UI/login_page.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -7,7 +9,226 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          margin: const EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 20,
+          ),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Loginpage(),
+                        ),
+                      );
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      size: 30,
+                      color: kBlack,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  const Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "Nama lengkap",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: kLightGray.withOpacity(0.3),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email or Phone number"),
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: kGray,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Email",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: kLightGray.withOpacity(0.3),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email or Phone number"),
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: kGray,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Create password",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: kLightGray.withOpacity(0.3),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Password",
+                  ),
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: kGray,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Confirm password",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: kLightGray.withOpacity(0.3),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Password",
+                  ),
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: kGray,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.all(2),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: TextButton(
+                  onPressed: () {
+                    print("data di submit");
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    minimumSize:
+                        Size.fromWidth(MediaQuery.of(context).size.width / 3),
+                    primary: kBlack,
+                    backgroundColor: kSecondary,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Create account',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: kBlack,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
