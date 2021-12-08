b3194fa3c69d12c7e4ed3cc74e32046baa0076c8
aldanrp <rallfirdan@gmail.com>
2021-12-08 20:43:28 +0700

    unused widget

--- lib/UI/Widget/HomeWidget/category_widget.dart
+++ lib/UI/Widget/HomeWidget/category_widget.dart
@@ -14,40 +14,45 @@ class Category extends StatelessWidget {
       ),
       child: DefaultTabController(
         length: categoryData.length,
-        child: TabBar(
-          labelPadding: const EdgeInsets.all(0),
-          indicator: BoxDecoration(
-            color: kPrimary,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          indicatorPadding: const EdgeInsets.all(5),
-          isScrollable: true,
-          labelColor: kWhite,
-          unselectedLabelColor: kLightGray,
-          labelStyle: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-          ),
-          unselectedLabelStyle: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-          ),
-          tabs: categorydata.map(
-            (e) {
-              return Tab(
-                child: Container(
-                  margin: const EdgeInsets.only(right: 23, left: 20),
-                  child: Text(
-                    e.name,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontFamily: "Monstserrat",
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TabBar(
+              labelPadding: const EdgeInsets.all(0),
+              indicator: BoxDecoration(
+                color: kPrimary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              indicatorPadding: const EdgeInsets.all(5),
+              isScrollable: true,
+              labelColor: kWhite,
+              unselectedLabelColor: kLightGray,
+              labelStyle: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+              tabs: categorydata.map(
+                (e) {
+                  return Tab(
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 23, left: 20),
+                      child: Text(
+                        e.name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Monstserrat",
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
-          ).toList(),
+                  );
+                },
+              ).toList(),
+            ),
+          ],
         ),
       ),
     );
