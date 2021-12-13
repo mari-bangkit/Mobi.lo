ab39ff7b21e5393d7cf36ed115fe5e69d327b664
aldanrp <rallfirdan@gmail.com>
2021-12-13 20:46:50 +0700

    update error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,28 +1,36 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 
 class Auth with ChangeNotifier {
-  void signup(String email, String password) async {
+  Future<void> signup(String email, String password) async {
     Uri url = Uri.parse(
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7xZNF2E-ZTf9Iquart56mbayUs6p_kFo",
     );
 
-    var response = await http.post(
-      url,
-      body: json.encode({
-        "email": email,
-        "password": password,
-        "returnSecureToken": true,
-      }),
-    );
+    try {
+      var response = await http.post(
+        url,
+        body: json.encode({
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        }),
+      );
 
-    print(json.decode(response.body));
+      var responseData = json.decode(response.body);
+
+      if (responseData['error'] != null) {
+        throw responseData['error']["message"];
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
-  void login(String email, String password) async {
+  Future<void> login(String email, String password) async {
     Uri url = Uri.parse(
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7xZNF2E-ZTf9Iquart56mbayUs6p_kFo",
     );
